f98f73919c91065d11a5a67da19aeb3138576e6e
AWeirdDev <aweirdscratcher@gmail.com>
2026-06-01 16:25:30 +0800

    tests: reorganize test

---
+++ tests/general.py
@@ -0,0 +1,21 @@
+"""General functionality test script."""
+
+from pprint import pprint
+
+from fast_flights import FlightQuery, Passengers, create_query, get_flights
+
+query = create_query(
+    flights=[
+        FlightQuery(
+            date="2025-12-01",
+            from_airport="MYJ",
+            to_airport="TPE",
+        ),
+    ],
+    seat="economy",
+    trip="one-way",
+    passengers=Passengers(adults=1),
+    language="zh-TW",
+)
+res = get_flights(query)
+pprint(res)
